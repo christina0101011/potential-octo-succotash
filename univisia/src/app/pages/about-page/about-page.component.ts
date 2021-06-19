e8c24eedc8175b554fb3613aee88459f88258d5b
christina0101011 <xkapiy@gmail.com>
2021-06-19 23:23:31 +0300

    About page

--- univisia/src/app/pages/about-page/about-page.component.ts
+++ univisia/src/app/pages/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
 import { MainServiceService } from 'src/app/main-service.service';
 
 @Component({
@@ -6,12 +7,23 @@ import { MainServiceService } from 'src/app/main-service.service';
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.scss']
 })
-export class AboutPageComponent implements OnInit {
+export class AboutPageComponent implements OnInit, OnDestroy {
+  public windowWidth = window.innerWidth;
+  public breakpoint = this.mainServiceService.breakpoint;
+  private resizeSubscription: Subscription = new Subscription;
 
   constructor(private mainServiceService: MainServiceService) { }
 
   ngOnInit(): void {
-    this.mainServiceService.currentPage.next('about')
+    this.mainServiceService.currentPage.next('about');
+    this.resizeSubscription = this.mainServiceService.onResize$
+      .subscribe(size => this.windowWidth = size.innerWidth);
+  }
+
+  ngOnDestroy() {
+    if (this.resizeSubscription) {
+      this.resizeSubscription.unsubscribe();
+    }
   }
 
 }
